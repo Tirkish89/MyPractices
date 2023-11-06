7f56d0393833c364ff99676aa9025b7bd1ab37fd
Tirkish M <tirkish1989@gmail.com>
2023-11-06 23:58:49 +0100

    TestAgain Uploaded

---
+++ src/TestPractice/TestAgain.java
@@ -0,0 +1,13 @@
+package TestPractice;
+
+public class TestAgain {
+    public static void main(String[] args) {
+
+        System.out.println("****************");
+
+        System.out.println(" Hello World!!!");
+
+        System.out.println("****************");
+        System.out.println(".....");
+    }
+}
