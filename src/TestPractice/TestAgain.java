bab0da3bc926ee0cbbe9f53724ef5439b654dbd9
Tirkish M <tirkish1989@gmail.com>
2023-11-07 00:34:08 +0100

    TestAgain Uploaded

--- src/TestPractice/TestAgain.java
+++ src/TestPractice/TestAgain.java
@@ -6,9 +6,11 @@ public class TestAgain {
         System.out.println("****************");
 
         System.out.println(" Hello World!!!");
+        System.out.println(" Hello Batch12!!!");
 
         System.out.println("****************");
         System.out.println(".....");
         System.out.println(".....");
+        System.out.println("Practice a lot...");
     }
 }
